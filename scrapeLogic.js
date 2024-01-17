675a0bf33528050d689758a9218b0dbe4b85320e
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2024-01-17 18:59:36 +0200

    Updated args

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 const scrapeLogic = async res => {
   // Launch the browser
   const browser = await puppeteer.launch({
+    args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"],
     executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath()
   })
 
