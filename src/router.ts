92cfe42091aeff9a16d487c91da589ea455ed096
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2024-03-17 15:40:21 +0200

    Fixed missing line

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,6 @@ import ScheduleController from "./controllers/ScheduleController"
 import GroupController from "./controllers/GroupController"
 import LecturerController from "./controllers/LecturerController"
 import RoomController from "./controllers/RoomController"
-import UserController from "./controllers/UserController"
 import cors from "cors"
 import { Router } from "express"
 
@@ -11,10 +10,13 @@ router.use(cors())
 
 // You must provide at least one of the following: Group, Lecturer, Room
 router.get("/api/schedule", ScheduleController.getSchedule)
+
 router.get("/api/groups", GroupController.getGroups)
 router.get("/api/groups/scrape", GroupController.scrapeGroups)
+
 router.get("/api/lecturers", LecturerController.getLecturers)
 router.get("/api/lecturers/scrape", LecturerController.scrapeLecturers)
+
 router.get("/api/rooms", RoomController.getRooms)
 router.get("/api/rooms/scrape", RoomController.scrapeRooms)
 
