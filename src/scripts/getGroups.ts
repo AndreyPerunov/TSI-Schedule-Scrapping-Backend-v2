8653bc96bd3d966361ea020c22bde75f8de91e76
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2024-06-14 00:45:08 +0300

    Rewrote script

--- src/scripts/getGroups.ts
+++ src/scripts/getGroups.ts
@@ -1,9 +1,15 @@
-import ScraperService from "../services/ScraperService"
+import Group from "../models/Group"
+import ScraperService from "services/ScraperService"
 
-console.log("📜 Scraping groups...")
 ScraperService.getGroups()
-  .then(() => {
-    console.log("📜 Finished group scraping✅")
+  .then(async groups => {
+    try {
+      await Group.saveGroups(groups)
+      console.log("📜 Finished group scraping✅")
+    } catch (error) {
+      console.log("📜 Failed tp scrape groups❌")
+      console.error(error)
+    }
   })
   .catch(error => {
     console.log("📜 Failed tp scrape groups❌")
